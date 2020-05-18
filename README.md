eb7bf04568a5cdc7432502d3b0ba6950da636e33
Javid-Xi <javidxi1012@163.com>
2020-05-18 23:59:49 +0800

    Update

--- README.md
+++ README.md
@@ -2,32 +2,11 @@
 ROS Car Driving Part Program.  
 Control Chip: STM32F103RCT6
 
-RoboCar系列博客: [Blogs](https://javid.cn/tag/robocar/)  
-正在不断更新中...
+## 1. 实物图(RoboCar N1)
+<div><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.5/Model/RoboCar-N1.jpg" width="80%" /></div> 
 
-## 1. 渲染图(Model)
+## 2. 渲染图(RoboCar)
+<div><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.5/Model/robocar1.jpg" width="80%" /></div>
 
-### 1.1 正面(Front)
-
-<div><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.2/Model/robocar1.jpg" width="80%" /></div> 
-
-### 1.2 背面(Back)
-
-<div><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.2/Model/robocar2.jpg" width="80%" /></div>
-
-### 1.3 底部(Bottom)
-
-<center><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.2/Model/robocar3.jpg" width="80%" style="border-radius:20px"/></center>
-
-### 1.4 侧面(Side)
-
-<table>
-	<tr>
-		<td><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.2/Model/robocar4.jpg"></td>
-		<td><img src="https://cdn.jsdelivr.net/gh/Javid-Xi/Auto-Navigation-RoboCar@1.2/Model/robocar5.jpg"></td>
-	</tr>
-</table>
-
-## 2. Development Environment
-### 2.1 Keil uVision5
+## 3. Development Environment
 [![](https://cdn.jsdelivr.net/gh/Javid-Xi/Intelligent-Security-Alarm-System/Photo/software/keil.jpg)](http://www.keil.com)
--- Robo/pid.c
+++ Robo/pid.c
@@ -1,10 +1,48 @@
-#include "pid.h"
+/**
+************************************************************
+* @file         pid.c
+* @brief        pid控制
+* @author       Javid
+* @date         2020-05-14
+* @version      1.0
+*
+***********************************************************/
 
+#include "pid.h"
 
 int16_t motor_kp = 130; //PID参数
 int16_t motor_ki = 0; //PID参数
 int16_t motor_kd = 30; //PID参数
 
+int16_t yaw_kp = 130; //PID参数
+int16_t yaw_ki = 0; //PID参数
+int16_t yaw_kd = 30; //PID参数
+
+/*************************************************
+* Function: Yaw_PidCtl
+* Description: 	电机A PID控制函数	
+* Parameter:  	spd_target:编码器速度目标值
+*				spd_current: 编码器速度当前值
+* Return: 电机PWM速度
+*************************************************/
+int16_t Yaw_PidCtl(int16_t yaw_target, int16_t yaw_current)
+{
+    static int16_t yaw_speed;
+    static int32_t bias, bias_last, bias_integral = 0;
+
+    bias = yaw_target - yaw_current;
+
+    bias_integral += bias;
+
+    if(bias_integral > PID_INTEGRAL_UP) bias_integral = PID_INTEGRAL_UP;
+    if(bias_integral < -PID_INTEGRAL_UP) bias_integral = -PID_INTEGRAL_UP;
+
+    yaw_speed += yaw_kp * bias * PID_SCALE + yaw_kd * (bias - bias_last) * PID_SCALE + yaw_ki * bias_integral * PID_SCALE;
+
+    bias_last = bias;
+
+    return yaw_speed;
+}
 
 /*************************************************
 * Function: Motor_PidCtl_A
@@ -43,7 +81,6 @@ int16_t Motor_PidCtl_B(int16_t spd_target, int16_t spd_current)
 {
     static int16_t motor_pwm_out;
     static int32_t bias, bias_last, bias_integral = 0;
-
 
     bias = spd_target - spd_current;
 
